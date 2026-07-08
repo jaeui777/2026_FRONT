aeb8226ce2ec93880e264a91597937a8f2dc9303
jaeui777 <jaeui777@gmail.com>
2026-07-08 17:56:19 +0900

    JS practice1

---
+++ Practice1/exam1.js
@@ -0,0 +1,33 @@
+let 이름 = prompt('손님의 이름을 입력하세요:')
+let 음료 = prompt('원하는 음료를 입력하세요:')
+let 수량 = prompt('주문 수량을 입력하세요:')
+
+console.log('[주문 접수]')
+console.log('이름')
+console.log(이름)
+
+console.log('메뉴')
+console.log(음료)
+
+console.log('수량')
+console.log(수량)
+
+let 사용자이름 = prompt('사용자의 이름을 입력하세요:')
+let 운동종류 = prompt('운동 종류를 입력하세요:')
+let 운동시간 = prompt('운동 시간(분)을 입력하세요:')
+
+let 분당칼로리 = 7
+let 칼로리 = 운동시간 * 분당칼로리
+
+console.log('[운동 기록]')
+console.log('이름')
+console.log(사용자이름)
+
+console.log('운동종류')
+console.log(운동종류)
+
+console.log('운동시간')
+console.log(운동시간)
+
+console.log('칼로리')
+console.log(칼로리)
