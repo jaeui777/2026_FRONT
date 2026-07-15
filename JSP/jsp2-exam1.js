ae545b4a9cc801d45ad3c611089ff1cc8dae25cf
jaeui777 <jaeui777@gmail.com>
2026-07-15 17:22:32 +0900

    update

--- JSP/jsp2-exam1.js
+++ JSP/jsp2-exam1.js
@@ -11,7 +11,7 @@
 삼각형 넓이 공식: 밑변 * 높이 / 2
 */
 
-/*
+
 let 밑변 = Number(prompt('밑변'))
 let 높이 = Number(prompt('높이'))
 let 삼각형넓이 = 밑변*높이 / 2
@@ -29,11 +29,9 @@ console.log(`삼각형넓이 : ${삼각형넓이}`)
 화씨 변환 공식: (섭씨 온도 * 9 / 5) + 32
 */
 
-/*
 let 섭씨온도 = prompt('섭씨온도')
 let 화씨 = (섭씨온도*9/5) + 32
 console.log(`화씨 : ${화씨}`)
-
 
 
 /*
@@ -52,29 +50,77 @@ console.log(`나이 : ${나이}`)
 키(cm)와 몸무게(kg)를 각각 prompt 함수로 입력받아 BMI 지수를 계산하여 콘솔에 출력하는 프로그램을 작성하시오.
 BMI 지수 공식: 몸무게(kg) / (키(m) * 키(m))
 *힌트: 키는 cm 단위로 입력받지만, 공식에는 m 단위가 사용됩니다. 키(cm) / 100을 통해 단위를 변환해야 합니다.
+*/
+let 키 = Number(prompt('키(cm)')) / 100
+let 몸무게 = Number(prompt('몸무게(kg)'))
+BMI = 몸무게 / (키 * 키)
 
+console.log(BMI)
+
+
+
+/*
 문제 5: 관리자 확인
 아이디와 이메일을 prompt 함수로 입력받습니다. 아이디가 'admin'이거나 또는 이메일이 'admin@test.com'이면 '관리자'를, 그렇지 않으면 '일반 사용자'를 콘솔에 출력하는 프로그램을 작성하시오.
+*/
 
+let 아이디 = prompt('아이디')
+let 이메일 = prompt('이메일')
+if (아이디 == 'admin' | 이메일 == 'admin@test.com') {
+    console.log('관리자');}
+else {console.log ('일반 사용자');}
+
+/*
 문제 6: 학점 변환기 (HTML 출력)
 100점 만점의 점수를 prompt 함수로 입력받아, 점수에 따라 아래와 같이 등급을 부여하고 HTML 문서에 <h2> 태그로 출력하는 프로그램을 작성하시오.
 90점 이상: 'A'
 80점 이상 90점 미만: 'B'
-70점 미만: 'C'
+70점 미만: 'C' */
 
+let 점수 = Number(prompt('점수'))
+if(점수 >= 90) {document.querySelector('점수').innerHTML = 'A'}
+else if(점수 >= 80) {document.querySelector('점수').innerHTML = 'B'}
+else {document.querySelector('점수').innerHTML = 'C'}
+
+
+/*
 문제 7: 청년 할인 이벤트 대상 확인
 나이를 prompt로 입력받아, 20대 (20세 이상 그리고 29세 이하)에 해당하는 경우 "이벤트 대상입니다."를, 그렇지 않으면 "이벤트 대상이 아닙니다."를 콘솔에 출력하는 프로그램을 작성하시오.
+*/
+let 나이 = Number(prompt('나이'))
+if(나이 >=20 && 나이 < 30) {console.log("이벤트 대상입니다")}
+else {console.log('이벤트 대상이 아닙니다.')}
 
+/*
 문제 8: '좋아요' 카운터 만들기
 '좋아요' 수를 저장하는 likeCount 변수를 3으로 선언하고, 증감 연산자(++)를 사용하여 변수의 값을 1 증가시킨 뒤, "좋아요: [숫자]" 형식으로 새로운 '좋아요' 수를 콘솔에 출력하는 코드를 작성하시오.
-let likeCount = 3;
+*/
 
+let likeCount = 3
+for ( likeCount = 3 ; likeCount < 5 ; likeCount++)
+    console.log(" 좋아요 : " + likeCount)
+/*
 문제 9: 할 일 목록에 항목 추가하기
-다음과 같이 할 일 목록이 담긴 todoList 배열이 있습니다. prompt를 사용하여 새로운 할 일을 입력받아 배열의 마지막에 추가한 후, 변경된 배열 전체를 콘솔에 출력하는 프로그램을 작성하시오.
-초기 배열: let todoList = ['장보기', '과제하기'];
-힌트: 배열에 요소를 추가할 때는 .push() 메소드를 사용합니다.
+다음과 같이 할 일 목록이 담긴 todoList 배열이 있습니다. prompt를 사용하여 새로운 할 일을 입력받아 배열의 마지막에 추가한 후, 변경된 배열 전체를 콘솔에 출력하는 프로그램을 작성하시오.*/
 
+/*
+초기 배열: let todoList = ['장보기', '과제하기'];
+힌트: 배열에 요소를 추가할 때는 .push() 메소드를 사용합니다. */
+
+let todoList = ['장보기', '과제하기'];
+let todoList1 = prompt('새로운 할 일을 입력하세요')
+todoList.push(todoList1);
+console.log(todoList)
+
+
+/*
 문제 10: 대기열의 마지막 사람 확인하기
 다음과 같이 대기자 명단이 담긴 waitingList 배열이 있습니다. 배열의 마지막에 있는 사람의 이름을 찾아 "마지막 대기자는 [이름]입니다." 형식으로 콘솔에 출력하는 프로그램을 작성하시오.
 초기 배열: let waitingList = ['김민준', '이서연', '박도윤', '최지우'];
 힌트: 배열의 마지막 요소 인덱스는 배열.length - 1 입니다. */
+
+let waitingList = ['김민준', '이서연', '박도윤', '최지우'];
+let 마지막대기자 = waitingList[waitingList.length-1]
+console.log(`마지막 대기자는 ${마지막대기자}입니다`);
+
+
